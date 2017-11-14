a2fb2de6c5f93f0529c104f11e0602045e836015
Ilich Arredondo <ilichh1@hotmail.com>
2017-11-14 15:19:14 -0600

    :V

---
+++ src/practicagitrigobonustrack/PracticaGitRigoBonusTrack.java
@@ -0,0 +1,19 @@
+/*
+ * Licencia pendiente, para cambiar la licencia ir a -> Tools | Templates
+ */
+package practicagitrigobonustrack;
+
+/**
+ *
+ * @author Ilich Arredondo
+ */
+public class PracticaGitRigoBonusTrack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+    
+}
